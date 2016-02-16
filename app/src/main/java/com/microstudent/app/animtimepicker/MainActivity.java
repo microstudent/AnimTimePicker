ac637a324335d63748c3ec770572bafb17c70a87
Leaves <455173472@qq.com>
2016-02-16 14:59:57 +0800

    暂时占坑，有时间继续填

--- app/src/main/java/com/microstudent/app/animtimepicker/MainActivity.java
+++ app/src/main/java/com/microstudent/app/animtimepicker/MainActivity.java
@@ -1,7 +1,9 @@
 package com.microstudent.app.animtimepicker;
 
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void close(View view) {
+        this.finish();
+    }
+
+    public void openTimePicker(View view) {
+        TimePickerDialog dialog  = new TimePickerDialog(this,null,24,60,true);
+        dialog.show();
     }
 }
